9918e9819c931997194e371d438357b0b814caf6
Stefano <snebo54@hotmail.com>
2023-12-23 23:42:01 +0100

    Formatted to match ruby conventions

--- lib/substring_f.rb
+++ lib/substring_f.rb
@@ -1,18 +1,18 @@
-def count_apparence(word, dict, result)
+def str_count(word, dict, result)
   n_word = []
   word.each_char do |char|
     word.length >= 3 ? (dict.include?(char) ? result[char] += 1 : nil) : nil
     n_word.push(char)
     dict.include?(n_word.join('')) ? result[n_word.join('')] += 1 : nil
   end
-  while n_word.length > 0
+  while not n_word.empty?
     n_word.shift
     dict.include?(n_word.join('')) ? result[n_word.join('')] += 1 : nil
   end
 end
 
 def substrings(words, dict)
-  result = Hash.new{0}
-  words.gsub(/[^a-z0-9\s]/i,'').split(' ').each {|i| count_apparence(i.downcase,dict,result)}
+  result = Hash.new{ 0 }
+  words.gsub(/[^a-z0-9\s]/i,'').split(' ').each { |i| str_count(i.downcase, dict, result) }
   return result
 end
